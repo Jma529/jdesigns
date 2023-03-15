2f36b2b0c9053728445bfe4bafa0566bde8d9704
Jill <ja@powerledger.io>
2023-03-15 12:18:16 +1000

    updates to mobile nav

--- js/functions.js
+++ js/functions.js
@@ -3,26 +3,30 @@ jQuery(document).ready(function () {
   mobile_menu = ".mobile-menu";
 
   // Callback function to OPEN the mobile menu
-  function openMobileMenu() {
-    $(mobile_menu_trigger).click(function () {
-      $(mobile_menu).addClass("open");
-    });
-  }
-  // Callback function to CLOSE the mobile menu
-  function closeMobileMenu() {
-    $(mobile_menu_trigger).click(function () {
-      $(mobile_menu).removeClass("open");
-    });
-  }
 
-  // Mobile menu show/hide on trigger click
-  $(mobile_menu_trigger).on("click", function (e) {
-    e.preventDefault();
-
-    if ($(mobile_menu).hasClass("open") === false) {
-      openMobileMenu();
-    } else if ($(mobile_menu).hasClass("open") === true) {
-      closeMobileMenu();
-    }
+  $(mobile_menu_trigger).click(function () {
+    $("body").toggleClass("slide-right");
   });
+  // function openMobileMenu() {
+  //   $(mobile_menu_trigger).click(function () {
+  //     $(mobile_menu).addClass("open");
+  //   });
+  // }
+  // // Callback function to CLOSE the mobile menu
+  // function closeMobileMenu() {
+  //   $(mobile_menu_trigger).click(function () {
+  //     $(mobile_menu).removeClass("open");
+  //   });
+  // }
+
+  // // Mobile menu show/hide on trigger click
+  // $(mobile_menu_trigger).trigger("click", function (e) {
+  //   e.preventDefault();
+
+  //   if ($(mobile_menu).hasClass("open") === false) {
+  //     openMobileMenu();
+  //   } else if ($(mobile_menu).hasClass("open") === true) {
+  //     closeMobileMenu();
+  //   }
+  // });
 });

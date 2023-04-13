9880ee1686f499601c874f0d7a423ab46566b89c
Jill Augustus <jma529@gmail.com>
2023-04-13 18:51:16 +1000

    Add footer layout and styles

--- footer.php
+++ footer.php
@@ -14,20 +14,34 @@
 	<footer id="colophon" class="site-footer">
 		<div class="wrapper-small">
 			<div class="footer-content">
-				<div class="site-info">
-					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jdesigns' ) ); ?>" class="">
-						<?php
-						/* translators: %s: CMS name, i.e. WordPress. */
-						printf( esc_html__( 'Proudly powered by %s', 'jdesigns' ), 'WordPress' );
-						?>
+				<div class="footer-top">
+					<div class="site-info">
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jdesigns' ) ); ?>" class="">
+							<?php
+							/* translators: %s: CMS name, i.e. WordPress. */
+							printf( esc_html__( 'Proudly powered by %s', 'jdesigns' ), 'WordPress' );
+							?>
+						</a>
+						<span class="sep"> | </span>
+							<?php
+							/* translators: 1: Theme name, 2: Theme author. */
+							printf( esc_html__( 'Theme: %1$s by %2$s.', 'jdesigns' ), 'jdesigns', '<a href="http://underscores.me/">Jill Augustus</a>' );
+							?>
+					</div><!-- .site-info -->
+					<div class="footer-col"></div>
+					<div class="site-logo footer-col">
+					<a href="<?php echo get_home_url(); ?>" class="site-logo">
+						<img class="logo" src="<?php echo get_template_directory_uri(); ?>/media/jdesigns-logo-ruby.svg" alt="<?php bloginfo( 'name' ); ?>" />
 					</a>
-					<span class="sep"> | </span>
-						<?php
-						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'jdesigns' ), 'jdesigns', '<a href="http://underscores.me/">Jill Augustus</a>' );
-						?>
-				</div><!-- .site-info -->
-				<nav class="footer-menu footer-col">
+					</div>
+					<div class="form footer-col">
+						<h3 class="h2 ruby">Intentional,<br> Effortless,<br> 
+						Designed For Living.</h3>
+					</div>
+				</div>
+			<hr>
+			<div class="footer-bottom">
+				<nav class="footer-menu col">
 					<ul class="nav-menu">
 						<li>
 							<a href="#">Instagram</a>
@@ -40,20 +54,14 @@
 						</li>
 					</ul>
 				</nav>
-				<div class="site-logo footer-col">
-				<a href="<?php echo get_home_url(); ?>" class="site-logo">
-					<img class="logo" src="<?php echo get_template_directory_uri(); ?>/media/jdesigns-logo-ruby.svg" alt="<?php bloginfo( 'name' ); ?>" />
-				</a>
-				</div>
-				<div class="form footer-col">
-					<h3 class="h2">Connect</h3>
-					<p>Sign up for our newsletter</p>
+				<div class="form col">
+					<p class="link-style">Stay in the know</p>
 					<?php get_template_part('template-parts/mailchimp-form'); ?>
-				</div>
-
 				</div>
 			</div>
 		</div>
+		</div>
+	</div>
 						
 
 	</footer><!-- #colophon -->
--- template-parts/mailchimp-form.php
+++ template-parts/mailchimp-form.php
@@ -1,10 +1,7 @@
 <div id="mc_embed_signup">
 <form action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-      <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
       <div class="mc-field-group">
-        <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-      </label>
         <input type="email" value="Enter Email" name="EMAIL" class="required email" id="mce-EMAIL">
       </div>
         <div id="mce-responses" class="clear">
@@ -16,5 +13,4 @@
           </div>
       </form>
       </div>
-      <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday'; }(jQuery));var $mcj = jQuery.noConflict(true);</script>
       <!--End mc_embed_signup-->

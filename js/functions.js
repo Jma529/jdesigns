8142ea10a15cfbca014dc4e3c7d1df61084b2222
Jill Augustus <jma529@gmail.com>
2023-05-31 08:33:21 +1000

    Style change-form and buttons

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,4 @@
 jQuery(document).ready(function () {
-  var mobile_menu_trigger = ".hamburger";
-  var mobile_menu = ".mobile-menu";
-
-  // Callback function to OPEN the mobile menu
-
-  $(mobile_menu_trigger).click(function () {
-    $(mobile_menu).toggleClass("slide-right");
-    console.log("I am checked!");
-  });
-  // function openMobileMenu() {
-  //   $(mobile_menu_trigger).click(function () {
-  //     $(mobile_menu).addClass("open");
-  //   });
-  // }
-  // // Callback function to CLOSE the mobile menu
-  // function closeMobileMenu() {
-  //   $(mobile_menu_trigger).click(function () {
-  //     $(mobile_menu).removeClass("open");
-  //   });
-  // }
-
-  // // Mobile menu show/hide on trigger click
-  // $(mobile_menu_trigger).trigger("click", function (e) {
-  //   e.preventDefault();
-
-  //   if ($(mobile_menu).hasClass("open") === false) {
-  //     openMobileMenu();
-  //   } else if ($(mobile_menu).hasClass("open") === true) {
-  //     closeMobileMenu();
-  //   }
-  // });
 
   // Slick Slider, single slide automatic
 
--- template-parts/mailchimp-form.php
+++ template-parts/mailchimp-form.php
@@ -1,22 +1,28 @@
 <!-- Begin Mailchimp Signup Form -->
 <div id="mc_embed_signup">
-    <form action="https://jinteriordesigns.us13.list-manage.com/subscribe/post?u=cd9e3bc9eafed67d380da0c3a&amp;id=c79f18d955&amp;f_id=007291e2f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <form
+        action="https://jinteriordesigns.us13.list-manage.com/subscribe/post?u=cd9e3bc9eafed67d380da0c3a&amp;id=c79f18d955&amp;f_id=007291e2f0"
+        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank"
+        novalidate>
         <div id="mc_embed_signup_scroll">
-<div class="mc-field-group">
-	<input type="email" value="ENTER EMAIL" name="EMAIL" class="required email" id="mce-EMAIL" onfocus="this.value=''" required>
-	<span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-</div>
-	<div id="mce-responses" class="clear foot">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cd9e3bc9eafed67d380da0c3a_c79f18d955" tabindex="-1" value=""></div>
-        <div class="optionalParent">
-            <div class="clear foot">
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+            <div class="mc-field-group">
+                <input type="email" value="ENTER EMAIL" name="EMAIL" class="required email" id="mce-EMAIL" autocomplete="
+                    email" onfocus="this.value=''" required>
+                <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+            </div>
+            <div id="mce-responses" class="clear foot">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
+                    name="b_cd9e3bc9eafed67d380da0c3a_c79f18d955" tabindex="-1" value=""></div>
+            <div class="optionalParent">
+                <div class="clear foot">
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                </div>
             </div>
         </div>
-    </div>
-</form>
+    </form>
 </div>
 <!--End mc_embed_signup-->

323215f7b9cc87d4f0ca7ba47b6c2e9fd887d6f4
Jill Augustus <jma529@gmail.com>
2024-07-14 13:23:57 +1000

    update contact page

--- templates/contact.php
+++ templates/contact.php
@@ -13,18 +13,19 @@ $tel = get_field('telephone');
 $location_1 = get_field('location_1');
 $location_2 = get_field('location_2');
 $contact_form = do_shortcode('[ninja_form id=1]');
+$image = get_the_post_thumbnail_url( get_the_ID(), 'full' );
 
 get_header();
 ?>
 
 <main id="primary" class="contact twocol">
   <section class="padding">
-  <div class="wrapper">
-      <h1><?php the_title(); ?></h1>
-      <hr>
+  <div class="wrapper large">
       <div class="grid">
-        <div class="col-1">
+        <div class="item-1 row-1">
           <div class="contact-info">
+          <h1 class="h3 bold"><?php the_title(); ?></h3>
+          <p class="bold">For General Inquiries</p>
             <a href="<?php echo esc_url( 'mailto:' . antispambot( $email )  ); ?>"><?php echo esc_html( antispambot( $email ) ); ?></a>
             <a href="tel:<?php echo $tel; ?>"><?php echo $tel; ?></a>
             <div class="location">
@@ -35,11 +36,12 @@ get_header();
             </div>
           </div>
         </div>
-        <div class="col-2">
-          <div class="contact-form">
-            <h2>Send us a message<h2>
-            <?php echo $contact_form; ?>
+        <div class="item-2 row-1">
+        <?php if( !empty($image)) : ?>
+          <div class="image">
+            <img src="<?= $image; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
           </div>
+          <?php endif; ?>
         </div>
       </div>
     </div>
